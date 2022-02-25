a9dfd71769775d88d8a3fa754a4b5f23033f9cbe
Leonardo-M94 <marick-2011@mail.ru>
2022-02-26 02:06:48 +0300

    HW: DB H2 connection

---
+++ spring-boot/HW/spring-boot-jdbc-hw/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO emails(id, email, password, FIO, birthday, male) VALUES
+(1, 'user1-Ivanov@mail.ru', 'r46RbUIN09', 'Ivanov Ivan Ivanovich', '1990-11-10', TRUE),
+(2, 'Ant.Petrov-95@yandex.ru', 'dki23ERS8323', 'Petrov Anton Mihaylovich', '1995-07-19', TRUE),
+(3, 'Kovaleva.Anna.Vas@gmail.com', 'fsds78REW78J', 'Kovaleva Anna Vasilyevna', '1989-01-23', FALSE),
+(4, 'Nik-Nik72@mail.ru', 'qwerty123Jpk', 'Nikonenko Nikolay Petrovich', '1972-10-15', TRUE);
