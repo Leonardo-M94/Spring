c0db9cbe644a58749f71a688bf42e47c2675a696
Leonardo-M94 <marick-2011@mail.ru>
2022-03-10 09:08:16 +0300

    Create 'Genre', 'Place' and set connections.

--- spring-boot/spring-boot-jpa-foundations/src/main/resources/data.sql
+++ spring-boot/spring-boot-jpa-foundations/src/main/resources/data.sql
@@ -1,3 +1,11 @@
-insert into films(title) values
-('Harry Potter'),
-('Back to the future');
+INSERT INTO GENRE(ID, NAME) VALUES
+(1, 'fantasy'), (2, 'fantastic');
+
+INSERT INTO films(ID, TITLE, GENRE_ID) VALUES
+(1, 'Harry Potter', 1),
+(2, 'Back to the future', 2);
+
+INSERT INTO PLACE(ID, NAME, FILM_ID) VALUES
+(1, 'London square', 1),
+(2, 'Train', 1),
+(3, 'USA', 2);
