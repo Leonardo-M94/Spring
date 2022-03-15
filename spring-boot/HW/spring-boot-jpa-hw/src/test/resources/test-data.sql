80375dc1a9db60f81b21177b55e42dd6fb2920b6
Leonardo-M94 <marick-2011@mail.ru>
2022-03-15 12:34:45 +0300

    Add tests for repositories

---
+++ spring-boot/HW/spring-boot-jpa-hw/src/test/resources/test-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO pets(id, breed) VALUES
+(1, 'Cat'),
+(2, 'Dog');
+
+INSERT INTO emails(id, email, password) VALUES
+(1, 'user1-Ivanov@mail.ru', 'r46RbUIN09'),
+(2, 'Ant.Petrov-95@yandex.ru', 'dki23ERS8323'),
+(3, 'Nik-Nik72@mail.ru', 'qwerty123Jpk');
+
+
+INSERT INTO users(id, fio, birthday, male, email_id, pet_id) VALUES
+(1, 'Ivanov Ivan Ivanovich', '1990-11-10', TRUE, 1, 1),
+(2, 'Petrov Anton Mihaylovich', '1995-07-19', TRUE, 2, 2);
